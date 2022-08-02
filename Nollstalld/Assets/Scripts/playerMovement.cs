d1f7c1a9e1257d94bb0b9e5eb226d9174ce5b4db
SofieMagnuson <60152591+SofieMagnuson@users.noreply.github.com>
2022-08-02 13:37:56 +0200

    StartUp

---
+++ Nollstalld/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody2D RB;
+    public CapsuleCollider2D playCol;
+    public SpriteRenderer SR;
+    public float playerSpeed, playerJump;
+    float horizontalMove;
+
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        SR = GetComponent<SpriteRenderer>();
+
+        playerSpeed = 3f;
+        horizontalMove = 0f;
+        playerJump = 5f;
+        isGrounded = false;
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        //if (!isShowingGlass)
+        {
+            transform.position += movement * Time.deltaTime * playerSpeed;
+        }
+
+
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            SR.flipX = false;
+        }
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            SR.flipX = true;
+
+        }
+        //hoppa
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                RB.velocity = new Vector2(RB.velocity.x, 0);
+                RB.AddForce(new Vector2(0, playerJump), ForceMode2D.Impulse);
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            RB.velocity = new Vector2(RB.velocity.x, 0);
+            RB.AddForce(new Vector2(0, playerJump), ForceMode2D.Impulse);
+            isGrounded = false;
+
+
+        }
+    }
+}
